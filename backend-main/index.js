a5c23d735075389a86e57d3e34f0f003db43c0f4
SouravGaur <167708256+SouravGaur@users.noreply.github.com>
2025-08-16 09:14:16 +0530

    final commit

--- backend-main/index.js
+++ backend-main/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 // app.use(cors());
 app.use(
   cors({
-    origin: "http://localhost:5174", // exact origin of your frontend
+    origin: "https://lzycrayproject.onrender.com", // exact origin of your frontend
     credentials: true,
   })
 );
